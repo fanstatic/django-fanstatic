d01e5d5dbce800abef8731800e4c29b5e7b96949
Jan-Jaap Driessen <devnull@localhost>
2011-03-26 15:36:36 +0100

    More metadata

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import sys, os
+
+version = '1.0dev'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read())
+
+setup(name='django_fanstatic',
+      version=version,
+      description='Fanstatic support for Django.',
+      long_description=long_description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Fanstatic Developers',
+      author_email='fanstatic@googlegroups.com',
+      url='http://fanstatic.org',
+      license='BSD',
+      packages=['django_fanstatic'],
+      package_dir = {'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'Django >= 1.2, <1.3',
+          'fanstatic',
+      ])
