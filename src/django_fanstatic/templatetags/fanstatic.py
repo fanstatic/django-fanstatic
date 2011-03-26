81246318de376cf58ef17893865aed3202c48e0c
Boaz Leskes <boaz@buzzcapture.com>
2011-03-26 21:07:01 +0100

    Add a resourceurl template tag. Single paramer with the following syntax  library_name:path_to_resource

---
+++ src/django_fanstatic/templatetags/fanstatic.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+import fanstatic
+from  django import template
+__author__ = 'boaz'
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def resourceurl(resource):
+    parsed_name = resource.split(u':',1)
+    if len(parsed_name) == 1:
+        raise template.TemplateSyntaxError(u"resource name must of the following syntax: library/rel_path. Got '%s'." %
+                                           (resource,))
+
+    fs_lib = fanstatic.get_library_registry().get(parsed_name[0])
+    needed = fanstatic.get_needed()
+    return needed.library_url(fs_lib)+"/"+parsed_name[1]
+  
