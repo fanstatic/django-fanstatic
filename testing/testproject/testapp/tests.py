2a30953b8e3fe34aaa45819aacacece47e54b48e
Boaz Leskes <boaz@buzzcapture.com>
2011-07-13 21:20:45 +0200

    Added a test for implied image url

--- testing/testproject/testapp/tests.py
+++ testing/testproject/testapp/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.test import TestCase
 
 class ResourceTests(TestCase):
@@ -5,5 +6,14 @@ class ResourceTests(TestCase):
         response = self.client.get('/')
         print response.content
 
-
+    def test_implied_image(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+        img_match = re.search('<img[^/]+src="(?P<url>[^"]+)"[^/]+/>',response.content)
+        self.assertTrue(img_match is not None)
+        img_url = img_match.group("url")
+        response = self.client.get(img_url)
+        self.assertEqual(response.status_code,200)
+        self.assertTrue(len(response.content)>0)
+        
 
