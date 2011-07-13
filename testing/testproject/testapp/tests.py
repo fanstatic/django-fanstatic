f7cf524c6f24dce12f1dca12a00ce69c2bd6f720
Remco Wendt <remco@maykinmedia.nl>
2011-07-13 20:56:02 +0200

    First version of tests

--- testing/testproject/testapp/tests.py
+++ testing/testproject/testapp/tests.py
@@ -1,23 +1,9 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class ResourceTests(TestCase):
+    def test_static_resource(self):
+        response = self.client.get('/')
+        print response.content
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
 
