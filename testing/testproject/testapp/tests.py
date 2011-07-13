c14cce6598a1f0ecdc29c85972aeac602d9cedee
Remco Wendt <remco@maykinmedia.nl>
2011-07-13 22:16:49 +0200

    Improved test

--- testing/testproject/testapp/tests.py
+++ testing/testproject/testapp/tests.py
@@ -1,16 +1,24 @@
 import re
+
+from django.conf import settings
 from django.test import TestCase
 
+
 class ResourceTests(TestCase):
+
     def test_css(self):
+        "Test the link injection for css done by fanstatic"
         response = self.client.get('/')
-        self.assertEqual(response.status_code,200)
-        css_match = re.search('<link[^>]+href="(?P<url>[^"]+)"[^/]+/>',response.content)
-        self.assertTrue(css_match is not None)
+
+        # The index page should exist
+        self.assertEqual(response.status_code, 200)
+
+        # The injected link should be present (only once)
+        css_match = re.search('<link[^>]+href="(?P<url>[^"]+)"[^/]+/>', response.content)
+        self.assertEquals(len(css_match.groups()), 1)
+
+        # The referred to css must exist
         css_url = css_match.group("url")
-
-
-        # The css must be present
         response = self.client.get(css_url)
         self.assertEqual(response.status_code,200)
         self.assertContains(response, '// a.css')
@@ -19,10 +27,30 @@ class ResourceTests(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code,200)
         img_match = re.search('<img[^/]+src="(?P<url>[^"]+)"[^/]+/>',response.content)
-        self.assertTrue(img_match is not None)
+        self.assertTrue(img_match)
         img_url = img_match.group("url")
         response = self.client.get(img_url)
         self.assertEqual(response.status_code,200)
-        self.assertTrue(len(response.content)>0)
-        
+        self.assertTrue(len(response.content)>0):w
+
+    def test_fanstatic_not_working_without_middleware(self):
+        """
+        Test that fanstatic doesn't do anything when the middleware is not
+        installed in the MIDDLEWARE_CLASSES setting.
+        """
+        old_middleware = settings.MIDDLEWARE_CLASSES
+
+        new_middleware = list(settings.MIDDLEWARE_CLASSES)
+        new_middleware.remove('django_fanstatic.FanstaticMiddleware')
+        settings.MIDDLEWARE_CLASSES = new_middleware
+
+        # The index page should exist
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        # The injected link should not be present (only once)
+        css_match = re.search('<link[^>]+href="(?P<url>[^"]+)"[^/]+/>', response.content)
+        self.assertFalse(css_match)
+
+        settings.MIDDLEWARE_CLASSES = old_middleware
 
